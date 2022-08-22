4ff5364179a9e04b1af6c86e53a175337185151d
Kaiya Hedley <keshedley@gmail.com>
2022-08-22 15:11:58 -0600

    starting of students/cohorts database

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,17 @@
+CREATE TABLE  Cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE Students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
